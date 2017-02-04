135480423583da1f4a377a6d86bebd1319984e22
Javier Ojeda <ojeda.javier94@gmail.com>
2017-02-04 02:52:34 -0600

    use pg as default database adapter

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170204011806) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "movies", force: :cascade do |t|
     t.string   "name"
     t.string   "director"
@@ -31,8 +34,8 @@ ActiveRecord::Schema.define(version: 20170204011806) do
     t.datetime "rent_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["movie_id"], name: "index_rents_on_movie_id"
-    t.index ["user_id"], name: "index_rents_on_user_id"
+    t.index ["movie_id"], name: "index_rents_on_movie_id", using: :btree
+    t.index ["user_id"], name: "index_rents_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -48,8 +51,8 @@ ActiveRecord::Schema.define(version: 20170204011806) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
 end
