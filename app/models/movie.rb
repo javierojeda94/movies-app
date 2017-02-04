75f8421dcfd157f5b0dd3523800034bfe07f4278
Javier Ojeda <ojeda.javier94@gmail.com>
2017-02-03 18:49:03 -0600

    change the minimum needed for the movie :available scope

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,5 +2,5 @@ class Movie < ApplicationRecord
   has_many :rents
   has_many :users, through: :rents
 
-  scope :available, -> { where('stock > 1') }
+  scope :available, -> { where('stock > 0') }
 end
